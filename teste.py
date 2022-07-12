f15bc646b5fb0672112e3b6c926901f0174c17f2
mattsandes <mateussandes@hotmail.com.br>
2022-07-12 00:09:55 -0300

    new way to see the code

--- teste.py
+++ teste.py
@@ -1,1 +1,11 @@
-print('Hello World!')
+nota1 = float(input(''))
+nota2 = float(input(''))
+
+media = nota1 + nota2 / 2
+
+print(f'Sua media é de {media}')
+
+if media < 7.0:
+    print('Reprovado')
+else:
+    print('Aprovado'
